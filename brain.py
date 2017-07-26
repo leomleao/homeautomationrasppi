c39da33ab41dc2f40565345603310456c1e3a854
Leonardo Leao <leomleao@gmail.com>
2017-07-26 01:50:39 +0000

    Fix problem of all GPIO sending signals, now only the command will send the signal

--- brain.py
+++ brain.py
@@ -5,50 +5,40 @@ import time
 #setting up the GPIOS, BCM indicates we're referring to the GPIO number, not the PIN number
 GPIO.setmode(GPIO.BOARD)
 
-#set all the GPIO to out mode
-
-#turn on feature GPIOs
-GPIO.setup(36, GPIO.OUT)
-GPIO.setup(38, GPIO.OUT)
-GPIO.setup(40, GPIO.OUT)
-
-
-#turn off feature GPIOs
-GPIO.setup(33, GPIO.OUT)
-GPIO.setup(35, GPIO.OUT)
-GPIO.setup(37, GPIO.OUT)
-
-
 try:
     if len(sys.argv) > 1:
         if sys.argv[1] == 'lights':
             if sys.argv[2] == 'on':
-              
+                
+                GPIO.setup(40, GPIO.OUT)
                 GPIO.output(40, True)
 
             elif sys.argv[2] == 'off':
-
+                
+                GPIO.setup(33, GPIO.OUT)
                 GPIO.output(33, True)
 
 
         elif sys.argv[1] == 'ac':
             if sys.argv[2] == 'on':
-
+                
+                GPIO.setup(38, GPIO.OUT)
                 GPIO.output(38, True)
 
             elif sys.argv[2] == 'off':
-        
+                
+                GPIO.setup(35, GPIO.OUT)
                 GPIO.output(35, True)
-
 
         elif sys.argv[1] == 'sunlight':
             if sys.argv[2] == 'on':
-
+                
+                GPIO.setup(36, GPIO.OUT)
                 GPIO.output(36, True)
 
-
             elif sys.argv[2] == 'off':
-
+                
+                GPIO.setup(37, GPIO.OUT)
                 GPIO.output(37, True)
 
 
